eaae9605885d48792817c47c54bd939af4ffa102
mxrain <mxrain@yeah.net>
2024-09-05 11:26:52 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        
       </nav>
     </div>
   </header>
