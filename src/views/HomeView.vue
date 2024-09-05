0020661ef32e34131b5e5dccee3f20507bc40dd3
mxrain <mxrain@yeah.net>
2024-09-05 13:57:44 +0800

    add

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,10 @@ import { ref,onMounted } from 'vue'
 
 const data = ref([])
 onMounted(async ()=>{
-  const res = await fetch('https://raw.githubusercontent.com/mxrain/indexNav/main/src/assets/data/data.json');
+  const res = await fetch(
+    // 'https://raw.githubusercontent.com/mxrain/indexNav/main/src/assets/data/data.json'
+    '@/assets/data/data.json'
+  );
   data.value = await res.json()
   console.log(data)
 })
